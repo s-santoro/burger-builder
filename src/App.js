5d970162e45577e3f0da8c931bb44e5466f3af0b
s-santoro <ssantoro@ntb.ch>
2019-05-31 19:01:58 +0200

    Imported component BurgerBuilder and used it in component Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Layout from './components/Layout/Layout';
-
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder/>
         </Layout>
       </div>
     );
