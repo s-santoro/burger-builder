bc3ad28cdcd90d9a72d1c98f1bd62000756ed6fb
s-santoro <ssantoro@ntb.ch>
2019-05-31 19:02:47 +0200

    Added css-module for Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
-
+import Style from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={Style.Content}>
             {props.children}
         </main>
     </Aux>
