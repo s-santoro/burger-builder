6c04d03a756d58436e59e9b5dcaeb49db70b7dd9
s-santoro <ssantoro@ntb.ch>
2019-05-31 21:38:00 +0200

    Added functionality to add or remove ingredients and show the summed up price of the burger. Ingredients can only be removed, when they exist (avoiding array-error index < 0)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+
+
+const INGREDIENT_PRICES = {
+  salad: 1,
+  cheese: 1.5,
+  bacon: 1.75,
+  meat: 2.4
+};
 
 class BurgerBuilder extends Component {
   constructor(props) {
@@ -12,15 +21,60 @@ class BurgerBuilder extends Component {
         bacon: 0,
         cheese: 0,
         meat: 0
-      }
+      },
+      totalPrice: 6
     }
   }
 
+  addIngredient = (type) => {
+    const oldInCount = this.state.ingredients[type];
+    const updatedCount = oldInCount + 1;
+    // create clone of ingredients for safer updating
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+    // update state
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+  };
+
+  removeIngredient = (type) => {
+    const oldInCount = this.state.ingredients[type];
+    if (oldInCount <= 0){
+      return;
+    }
+    const updatedCount = oldInCount - 1;
+    // create clone of ingredients for safer updating
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+    // update state
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+  };
+
   render() {
+
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+    // creates array of {salad: true, bacon: true, cheese: true, meat: true} to track which button should
+    // be disabled
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients }/>
-        <div>Burger Controls</div>
+        <BuildControls
+          addIngredient={this.addIngredient}
+          removeIngredient={this.removeIngredient}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+        />
       </Aux>
     );
   }
