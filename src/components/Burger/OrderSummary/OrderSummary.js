c657ebcce94d16d6a7825d26804d4a9b8289cce6
s-santoro <ssantoro@ntb.ch>
2019-06-09 20:38:56 +0200

    Converted functional components to class-based components and used lifecycle hooks to only re-render modal content when modal is shown.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Button from '../../UI/Button/Button';
 import Aux from '../../../hoc/Aux';
 
-const orderSummary = (props) => {
+/**
+ * Lifecylce Hooks were used, to improve performance of rendering.
+ *
+ */
+class OrderSummary extends Component {
 
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={igKey}>
+  // Used to check when OrderSummary will be updated.
+  // As soon as ingredients are added, Modal updates OrderSummary even though it's not shown.
+  // Only updating OrderSummary when Modal will be shown, improves performance.
+  componentWillUpdate(nextProps, nextState, nextContext) {
+    console.log("[OrderSummary] WillUpdate");
+  }
+
+  render() {
+
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
           <span style={{textTransform: 'capitalize'}}>{igKey}
-          </span>: {props.ingredients[igKey]}
-        </li>
-      );
-    })
+          </span>: {this.props.ingredients[igKey]}
+          </li>
+        );
+      });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p>Total Price: <strong>{props.totalPrice.toFixed(2)} $</strong></p>
-      <p>Continue with Checkout?</p>
-      <Button btnType={"Danger"} clicked={props.cancelOrder}>CANCEL</Button>
-      <Button btnType={"Success"} clicked={props.continueOrder}>CONTINUE</Button>
-    </Aux>
-  );
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p>Total Price: <strong>{this.props.totalPrice.toFixed(2)} $</strong></p>
+        <p>Continue with Checkout?</p>
+        <Button btnType={"Danger"} clicked={this.props.cancelOrder}>CANCEL</Button>
+        <Button btnType={"Success"} clicked={this.props.continueOrder}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
 
-
-};
-
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.module.css';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.cancelOrder}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+  // Set condition to only re-render Modal when show is changed
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate(nextProps, nextState, nextContext) {
+    console.log("[Modal] WillUpdate");
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.cancelOrder}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+
+}
+
+export default Modal;
