40e48644e51e9a9b3da3328469ee45488d2dd77b
s-santoro <ssantoro@ntb.ch>
2019-07-25 22:17:31 +0200

    form with firebase submit

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,16 +2,59 @@ import React, {Component} from 'react';
 import Axios from '../../../Axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
-
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.module.css';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      zipCode: '',
+    orderDetails: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-Mail'
+        },
+        value: ''
+      },
+      address: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Address'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Postal Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Country'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: ['standard', 'express', 'prime']
+        },
+        value: ''
+      },
     },
     loading: false
   };
@@ -20,13 +63,15 @@ class ContactData extends Component {
     // avoid page reloading by form submission
     event.preventDefault();
     this.setState({ loading: true });
+    // create key-value of orderDetails {"email": "test@test.com"}
+    let orderDetails = {};
+    for(let key in this.state.orderDetails) {
+      orderDetails[key] = this.state.orderDetails[key].value;
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: "John Doe",
-        email: "test@test.com"
-      }
+      orderDetails: orderDetails
     };
     Axios
       .post("/orders.json", order)
@@ -42,16 +87,42 @@ class ContactData extends Component {
       });
   };
 
+  changeHandler = (event, inputIdentifier) => {
+    // create deep-clone of orderDetails by first creating a shallow clone
+    let newOrderDetails = {...this.state.orderDetails};
+    // then create shallow clone of the correct input (i.e. email)
+    let detailInfo = {...newOrderDetails[inputIdentifier]};
+    // then change value of new input clone
+    detailInfo.value = event.target.value;
+    // then change input from orderDetails clone with the updated input
+    newOrderDetails[inputIdentifier] = detailInfo;
+    // finally set state
+    this.setState({orderDetails: newOrderDetails});
+  };
+
   render() {
+    let formArray = [];
+    for (let key in this.state.orderDetails) {
+      formArray.push({
+        id: key,
+        config: this.state.orderDetails[key]
+      });
+    }
     let form = (
-      <form>
-        <input type="text" name="name" placeholder="your name"/>
-        <input type="email" name="email" placeholder="your email"/>
-        <input type="text" name="street" placeholder="your street"/>
-        <input type="text" name="zipCode" placeholder="your zip code"/>
+      <form onSubmit={this.orderHandler}>
+        {formArray.map(item => {
+          return (
+            <Input
+              key={item.id}
+              elementType={item.config.elementType}
+              elementConfig={item.config.elementConfig}
+              value={item.config.value}
+              changed={(event) => {this.changeHandler(event, item.id)}}
+            />
+          );
+        })}
         <Button
           btnType={"Success"}
-          clicked={this.orderHandler}
         >Order</Button>
       </form>
     );
